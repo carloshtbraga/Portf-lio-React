1698d13f5a8f0e5b032f115b525377509596c0ec
Carlos <carloshtbraga@gmail.com>
2023-02-27 01:09:20 -0300

    oi

--- src/services/variables.jsx
+++ src/services/variables.jsx
@@ -8,16 +8,20 @@ import JS from "../img/java-script.png";
 import Reacti from "../img/structure.png";
 import Git from "../img/git.png";
 import Docker from "../img/docker.png";
+import TailWind from "../img/tailwind.png";
+import Jest from "../img/jest.png";
 
 export const technologies = [
-    { name: "HTML5", icon: HTML, type: "Front-end" },
-    { name: "CSS3", icon: CSS, type: "Front-end" },
-    { name: "JavaScript", icon: JS, type: "Language" },
-    { name: "Node", icon: Node, type: "Back-end" },
-    { name: "GitHub", icon: GitHub, type: "Outros" },
-    { name: "React", icon: Reacti, type: "Front-end" },
-    { name: "Sass", icon: Sass, type: "Front-end" },
-    { name: "Docker", icon: Docker, type: "Back-end" },
-    { name: "Git", icon: Git, type: "Outros" },
-    { name: "Python", icon: Python, type: "Language" },
-  ];
+  { name: "Node", icon: Node, type: "Back-end" },
+  { name: "Docker", icon: Docker, type: "Back-end" },
+  { name: "React", icon: Reacti, type: "Front-end" },
+  { name: "Python", icon: Python, type: "Language" },
+  { name: "Jest", icon: Jest, type: "Outros" },
+  { name: "GitHub", icon: GitHub, type: "Outros" },
+  { name: "Sass", icon: Sass, type: "Front-end" },
+  { name: "TailWind", icon: TailWind, type: "Front-end" },
+  { name: "HTML5", icon: HTML, type: "Front-end" },
+  { name: "CSS3", icon: CSS, type: "Front-end" },
+  { name: "JavaScript", icon: JS, type: "Language" },
+  { name: "Git", icon: Git, type: "Outros" },
+];
